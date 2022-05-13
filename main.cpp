ad225b6be9b1ab871eb76502cd3eaf222dd0bd4b
M0rdvil <92626084+M0rdvil@users.noreply.github.com>
2022-05-13 14:57:43 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,97 @@
-#include <iostream>
-#include <stdlib.h>
-#include<chrono>
+﻿#include<iostream>
+
 using namespace std;
 
-int main()
+int counterIterations = 0;
+int counterSwap = 0;
+const int nmax = 100000;
+
+void Merge(int arr[], int begin, int end)
 {
-	int n = 0;
-	cout << "" << endl;
-	cin >> n;
-	int* A = new int[n];
+	int i = begin;
+	int	t = 0;
+	int	mid = begin + (end - begin) / 2;
+	int	j = mid + 1;
+	int	d[nmax];
 
-	auto start = chrono::steady_clock::now();
-	for (int i = 0; i <= 0; i++)
-	{
-		A[i] = rand();
-
-	}
-	cout << endl << endl;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i; j > 0 && A[j-1] > A[j]; j--)
-		{
-			swap(A[j - 1], A[j]);
+	while (i <= mid && j <= end) {
+		counterIterations++;
+		if (arr[i] <= arr[j]) {
+			counterIterations++;
+			d[t] = arr[i];
+			counterSwap++;
+			i++;
 		}
-		auto end = chrono::steady_clock::now();
-		auto diff = end - start;
-		cout << chrono::duration <double, milli>(diff).count() << "ms" << endl;
-
-
+		else {
+			d[t] = arr[j];
+			counterSwap++;
+			j++;
+		}
+		t++;
 	}
+
+	while (i <= mid) {
+		counterIterations++;
+		d[t] = arr[i];
+		counterSwap++;
+		i++;
+		t++;
+	}
+
+	while (j <= end) {
+		counterIterations++;
+		d[t] = arr[j];
+		counterSwap++;
+		j++;
+		t++;
+	}
+
+	for (i = 0; i < t; i++) {
+		counterIterations++;
+		arr[begin + i] = d[i];
+		counterSwap++;
+	}
+}
+
+void MergeSort(int* arr, int left, int right)
+{
+	if (left < right) {
+		counterIterations++;
+		if (right - left == 1) {
+			counterIterations++;
+			if (arr[right] < arr[left]) {
+				counterIterations++;
+				swap(arr[left], arr[right]);
+			}
+		}
+		else {
+			MergeSort(arr, left, left + (right - left) / 2);
+			MergeSort(arr, left + (right - left) / 2 + 1, right);
+			Merge(arr, left, right);
+		}
+	}
+}
+
+
+
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n;
+	cout << " Введите количество: ";
+	cin >> n;
+	int arr[nmax];
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % 100;
+	}
+	
+	cout << endl;
+	unsigned int start_time = clock();
+	MergeSort(arr, 0, n - 1);
+	unsigned int end_time = clock();
+	unsigned int search_time = start_time - end_time;
+
+	cout << endl;
+	cout << "Time: " << search_time << endl;
+	cout << "Swaps + Iterations: " << counterSwap + counterIterations;
 }
